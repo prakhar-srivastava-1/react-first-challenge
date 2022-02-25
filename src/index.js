8ee7932cce3432329eb4d2c024ce51281d7651b2
Prakhar Srivastava <50711734+prakhar-srivastava-1@users.noreply.github.com>
2022-02-26 04:14:25 +0530

    create listItems JS array and inject the values in li tag // Inject list items as js const listItems = ["Milk", "Eggs", "Chicken"];
     <ul>
          <li>{listItems[0]}</li>
          <li>{listItems[1]}</li>
          <li>{listItems[2]}</li>
        </ul>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
-import React from "react";
-import ReactDOM from "react-dom";
+
+// Inject list items as js
+const listItems = ["Milk", "Eggs", "Chicken"];
 
 ReactDOM.render(
   <div>
     <h1>My List</h1>
     <ul>
-      <li>Milk</li>
-      <li>Eggs</li>
-      <li>Chicken</li>
+      <li>{listItems[0]}</li>
+      <li>{listItems[1]}</li>
+      <li>{listItems[2]}</li>
     </ul>
   </div>,
   document.getElementById("root")
